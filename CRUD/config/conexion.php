bcf042fca0c4bc26c0218b6cf7c2b09529e5d46d
Neo-Uli <124014493+Neo-Uli@users.noreply.github.com>
2023-09-22 21:54:54 -0600

    Update conexion.php

--- CRUD/config/conexion.php
+++ CRUD/config/conexion.php
@@ -4,7 +4,7 @@ private $usuario="root";
 private $clave="root";
 private $bd="accesos";
 
-private $servidor="app.dlopez.com";
+private $servidor="localhosts";
 public $sql;public $res;public $conector;
 public function __Construct(){
 $this->conector=new mysqli($this->servidor,$this->usuario,
